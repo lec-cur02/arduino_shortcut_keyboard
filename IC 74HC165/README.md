cb640b70f2f92a99857c2ebb858dda29436b2b1b
ramu12 <hoanglam121106@gmail.com>
2025-07-30 09:26:41 -0700

    add README file, comment code

---
+++ IC 74HC165/README.md
@@ -0,0 +1,28 @@
+#Arduino Marcopad keyboard 
+
+## Introduction:
+This project creates a Marcopad keyboard using Arduino uno 
+
+## Feature:
+- Multiple buttons used as input by shift register 74HC165
+- When pushing the button, character will appear in LCD
+
+## Hardware:
+- Arduino uno 
+- 2 x 74HC165 shift register(You can expand the keyboard with more register and buttons)
+- 20x4 LCD
+- Some suitable register
+
+## Circuit 
+![Wiring Diagram](images/circuit.png)
+
+## Setup
+1. Clone this repository
+2. Open with VSCode + PlatformIO
+3. Connect your Arduino
+4. Install required libraries:
+   - LiquidCrystal
+5. Upload the code
+
+## License 
+This project is licensed under the MIT License.
--- IC 74HC165/src/main.cpp
+++ IC 74HC165/src/main.cpp
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
 
+/*LCD in Four bits mode 
+RS = 13, RW to GND, E = 12, D4 = 5, D5 = 4, D6 = 3, D7 = 2  */
 LiquidCrystal lcd(13, 12, 5, 4, 3, 2);
 
 const int RS = 13; 
@@ -12,6 +14,8 @@ const int dataPin2 = A0;
 
 const char alphabet[14] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 
                          'I', 'J', 'K', 'L', 'M', 'N'};
+
+//cursor position
 int posX = 0, posY = 0;
 
 void setup(){
@@ -24,9 +28,11 @@ void setup(){
 }
 
 void loop(){
+// Load parallel data from shift registers to Q registers
   digitalWrite(latchPin, LOW);
   digitalWrite(latchPin, HIGH);
   for(int i=0; i < 8; i++){
+    //set new position of cursor when character printed
     lcd.setCursor(posX, posY);
     int val1 = digitalRead(dataPin1);
     int val2 = digitalRead(dataPin2);
@@ -38,10 +44,12 @@ void loop(){
       lcd.print(alphabet[15-i]);
       posX++;
     }
+    // Move to next line if end of line is reached
     if(posX == 20){
       posX = 0;
       posY++;
     }
+    //Shift to next bit
     digitalWrite(clockPin, HIGH);
     digitalWrite(clockPin, LOW);
   }
